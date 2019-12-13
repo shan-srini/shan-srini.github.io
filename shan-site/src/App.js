f3fe60e5284ae0cc92a9a4fd620632475428c494
Shanmukha Srinivasan <shanmukha@Shanmukhas-MacBook-Pro-2.local>
2019-12-12 23:33:26 -0500

    Added ScrutinyFB

--- shan-site/src/App.js
+++ shan-site/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
-import { HashRouter, Route, Switch} from "react-router-dom"
+import { HashRouter, Route, Switch } from "react-router-dom"
 // import logo from './logo.svg'
 import './App.css'
 
 import Home from './Pages/Home/Home'
 import NotFound from './Pages/NotFound/NotFound'
 
+//ScrutinyFB
+import ScrutinyFBPrivacyPolicy from './Pages/ScrutinyFB/Privacy_Policy/ScrutinyFB_PrivacyPolicy'
+import ScrutinyFBHelp from './Pages/ScrutinyFB/Help/ScrutinyFB_Help'
+
 class App extends Component {
 
   render() {
     return (
-      <HashRouter>
+      <HashRouter basename="/">
         <Switch>
           <Route exact path="/" component={Home} />
+
+          <Route exact path="/ScrutinyFB/Help" component={ScrutinyFBHelp} />
+          <Route exact path="/ScrutinyFB/PrivacyPolicy" component={ScrutinyFBPrivacyPolicy} />
+
           <Route component={NotFound} />
         </Switch>
       </HashRouter>
