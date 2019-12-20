91ce20a78b7e8b3414bc5fe3a5dba9c61cedc1f1
Shanmukha Srinivasan <shanmukha@Shanmukhas-MacBook-Pro-2.local>
2019-12-19 23:54:26 -0500

    Saving for progress, closer to v1 premise this time...

--- shan-site/src/Pages/Home/AnimateHeading.js
+++ shan-site/src/Pages/Home/AnimateHeading.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Home.css'
 
 class AnimateHeading extends Component {
@@ -13,32 +13,33 @@ class AnimateHeading extends Component {
     }
 
     componentDidMount() {
+        this.setState((prevState) => ({ showText: this.props.headingText.substring(0, prevState.startIndex + 1) }))
         this.animate(this.state.startIndex)
     }
 
     animate(n) {
         //display full text
-        if(n===this.props.headingText.length) {
-            this.setState({forward: false})
+        if (n === this.props.headingText.length) {
+            this.setState({ forward: false })
             setTimeout(() => this.updateShowText(n), 2000)
         }
         //display hi
-        else if(n===this.state.startIndex) {
-            this.setState({forward: true})
+        else if (n === this.state.startIndex) {
+            this.setState({ forward: true })
             setTimeout(() => this.updateShowText(n), 1000)
         }
-        else if (n<this.props.headingText.length) {
+        else if (n < this.props.headingText.length) {
             setTimeout(() => this.updateShowText(n), 100)
         }
     }
 
     updateShowText(n) {
-        var updateText = this.props.headingText.substring(0, n+1)
-        this.setState({showText: updateText})
+        var updateText = this.props.headingText.substring(0, n + 1)
+        this.setState({ showText: updateText })
         if (this.state.forward)
-            this.animate(n+1)
+            this.animate(n + 1)
         else
-            this.animate(n-1)
+            this.animate(n - 1)
     }
 
 
