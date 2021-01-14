652ab004855d50f2fc60656306e8222f95f28a4a
Shan Srinivasan <s.shan.srini@gmail.com>
2021-01-13 20:41:36 -0500

    init: Reset site for v2

--- shan-site/src/App.js
+++ shan-site/src/App.js
@@ -1,31 +1,12 @@
-import React, { Component } from 'react'
-import { HashRouter, Route, Switch } from "react-router-dom"
-// import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom';
 
-import PersonalSite from './Pages/PersonalSite/PersonalSite'
-import NotFound from './Pages/NotFound/NotFound'
-
-//ScrutinyFB
-import ScrutinyFBPrivacyPolicy from './Pages/ScrutinyFB/Privacy_Policy/ScrutinyFB_PrivacyPolicy'
-import ScrutinyFBHelp from './Pages/ScrutinyFB/Help/ScrutinyFB_Help'
-
-class App extends Component {
-
-  render() {
-    return (
-      <HashRouter basename="/">
-        <Switch>
-          <Route exact path="/" component={PersonalSite} />
-
-          <Route exact path="/ScrutinyFB/Help" component={ScrutinyFBHelp} />
-          <Route exact path="/ScrutinyFB/PrivacyPolicy" component={ScrutinyFBPrivacyPolicy} />
-
-          <Route component={NotFound} />
-        </Switch>
-      </HashRouter>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      APP
+    </div>
+  )
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('app'));
