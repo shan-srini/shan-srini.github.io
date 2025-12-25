beb66024f2e8216ccd21085ec4df0b749cd29517
shan-srini <s.shan.srini@gmail.com>
2025-12-24 23:27:35 -0500

    should be my final edits for now

--- src/components/VinylPlayer.tsx
+++ src/components/VinylPlayer.tsx
@@ -33,9 +33,9 @@ const VinylPlayer: React.FC<VinylPlayerProps> = ({ currentRecord, isPlaying }) =
     }, [isPlaying, currentRecord.coverImages]);
 
     return (
-        <div className="relative flex flex-col items-center justify-center py-10 w-full max-w-full mx-auto overflow-hidden px-4">
+        <div className="relative flex flex-col items-center justify-center py-10 w-full max-w-4xl mx-auto overflow-hidden px-4">
             {/* Turntable Base */}
-            <div className="absolute inset-x-[-10%] md:inset-x-[-20%] top-0 bottom-[-20px] bg-[#1a1a1a] rounded-lg border border-white/5 shadow-2xl z-0" />
+            <div className="absolute inset-x-0 top-0 bottom-[-20px] bg-[#1a1a1a] rounded-lg border border-white/5 shadow-2xl z-0" />
 
             {/* The Platter */}
             <div className="relative z-10 w-80 h-80 md:w-96 md:h-96 rounded-full bg-black border-4 border-[#222] shadow-[0_0_50px_rgba(0,0,0,0.5)] flex items-center justify-center overflow-hidden">
@@ -71,12 +71,20 @@ const VinylPlayer: React.FC<VinylPlayerProps> = ({ currentRecord, isPlaying }) =
 
             {/* Tonearm */}
             <motion.div
-                animate={{ rotate: isPlaying ? 25 : 0 }}
+                animate={{ rotate: isPlaying ? 30 : 5 }}
                 transition={{ type: "spring", stiffness: 50 }}
-                className="absolute right-[-10%] top-[10%] z-10 origin-top-right pointer-events-none"
+                className="absolute left-[calc(65%+150px)] md:left-[calc(50%+220px)] top-[8%] z-20 origin-top-right pointer-events-none"
             >
-                <div className="w-1 h-48 bg-zinc-600 rounded-full relative">
-                    <div className="absolute bottom-0 right-[-14px] w-8 h-12 bg-zinc-800 rounded-sm border border-zinc-700 shadow-md transform rotate-[-15deg]" />
+                <div className="w-2 h-[200px] bg-zinc-500 rounded-full relative shadow-lg">
+                    {/* Pivot point */}
+                    <div className="absolute top-0 right-0 w-12 h-12 bg-zinc-800 rounded-full -translate-y-1/2 translate-x-1/2 border-2 border-zinc-700 shadow-xl flex items-center justify-center">
+                        <div className="w-4 h-4 bg-zinc-600 rounded-full border border-zinc-500" />
+                    </div>
+
+                    {/* Headshell */}
+                    <div className="absolute bottom-0 right-[-16px] w-10 h-14 bg-zinc-800 rounded-sm border border-zinc-700 shadow-xl transform rotate-[-15deg]">
+                        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-5 h-8 bg-zinc-700/50 rounded-sm border border-zinc-600/30" />
+                    </div>
                 </div>
             </motion.div>
 
